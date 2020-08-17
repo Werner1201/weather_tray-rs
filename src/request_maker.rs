f4a8c91e4209b249da9ea7a1c75d4d5bbd00274d
Werner1201 <werner.romling@unigranrio.br>
2020-08-17 17:35:50 -0300

    Changed the location to my City to do some testing and Created a command line to set the envionment variable on Windows in the mean while

--- src/request_maker.rs
+++ src/request_maker.rs
@@ -1,22 +1,23 @@
-use std::{collections::HashMap, error::Error, env};
+use std::{collections::HashMap, env, error::Error};
 
 // Fetching data from Open weather API
 fn make_request() -> Result<HashMap<String, serde_json::Value>, Box<dyn std::error::Error>> {
-    let location = "Isbergues, FR";
+    let location = "Duque de Caxias";
     // $env:OPENWEATHER_API_KEY="<paste key>"; cargo run
     let app_id = env::var("OPENWEATHER_API_KEY")?;
     let url = format!(
         "http://api.openweathermap.org/data/2.5/weather?q={}&APPID={}&units=metric",
         location, app_id
     );
-    let resp = reqwest::blocking::get(&url)?
-        .json::<HashMap<String, serde_json::Value>>()?;
+    let resp = reqwest::blocking::get(&url)?.json::<HashMap<String, serde_json::Value>>()?;
     println!("{:#?}", resp["main"]["temp"]);
     Ok(resp)
 }
 
 // Transforming API result to i64 temperature (° Celsius)
 pub fn get_temp() -> Result<String, Box<dyn Error>> {
-    let temp = &make_request()?["main"]["temp"].as_f64().ok_or("Cannot convert temperature")?;
+    let temp = &make_request()?["main"]["temp"]
+        .as_f64()
+        .ok_or("Cannot convert temperature")?;
     Ok((*temp as i64).to_string())
 }
