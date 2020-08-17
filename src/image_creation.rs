0eb58566ac93ad7eb351cf129a1ea6dd06f0dabc
Nicolas BAUW <nbauw@hotmail.com>
2020-08-17 07:56:26 +0200

    Returning icon as Vec<u8>

--- src/image_creation.rs
+++ src/image_creation.rs
@@ -1,18 +1,17 @@
-use image::{Rgb, RgbImage};
+use image::{Rgba, DynamicImage, ImageOutputFormat};
 use imageproc::drawing::draw_text_mut;
 use rusttype::{Font, Scale};
-use std::path::Path;
 
 pub fn cria_imagem(value: &str) {
     //aqui ele atribui o caminho ao local onde foram enviados os argumentos
-    let path = Path::new("temp.ico");
-    let image = RgbImage::new(256, 256);
+    //let path = Path::new("temp.ico");
+    let image = DynamicImage::new_rgb8(256, 256);
 
     //aqui se salva a imagem no path definido
-    escreve_texto(image, value).save(path).unwrap();
+    escreve_texto(image, value);
 }
 
-fn escreve_texto(mut img: RgbImage, temp: &str) -> RgbImage {
+fn escreve_texto(mut img: DynamicImage, temp: &str) -> Vec<u8> {
     let font = Vec::from(include_bytes!("docs/DejaVuSans.ttf") as &[u8]);
     let font = Font::try_from_vec(font).unwrap();
 
@@ -25,12 +24,14 @@ fn escreve_texto(mut img: RgbImage, temp: &str) -> RgbImage {
     //Aqui se desenha o texto na imagem com essa funcao
     draw_text_mut(
         &mut img,
-        Rgb([255u8, 255u8, 255u8]),
+        Rgba([255u8, 255u8, 255u8, 255u8]),
         0,
         0,
         scale,
         &font,
         &format!("{}°", temp).to_string(),
     );
-    img
+    let mut icondata = Vec::new();
+    img.write_to(& mut icondata, ImageOutputFormat::Ico).unwrap();
+    icondata
 }
