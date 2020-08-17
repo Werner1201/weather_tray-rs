fd2dba04a09452d0bd0f7b43b55876acc6875d49
Nicolas BAUW <nbauw@hotmail.com>
2020-08-17 16:21:39 +0200

    Reactivated refresh menu (auto update maybe later)

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,18 @@ fn main() -> Result<(), systray::Error> {
         Err(_) => panic!("Can't create window!"),
     }
 
+    // At app init : we create systray icon (TODO : error handling)
     let icon = image_creation::create_icon().unwrap();
     app.set_icon_from_buffer(&icon[0..icon.len()], 256, 256).expect("Cannot set icon");
 
-    /*app.add_menu_item("Refresh", |window| {
-        if let Ok(temp) = request_maker::get_temp() {
-            image_creation::cria_imagem(&temp);
-        }
-        window.set_icon_from_file(&"temp.ico".to_string())?;
+    // Refresh : we recreate systray icon (TODO : automatic update ?)
+    app.add_menu_item("Refresh", |window| {
+        let icon = image_creation::create_icon().unwrap();
+        window.set_icon_from_buffer(&icon[0..icon.len()], 256, 256).expect("Cannot set icon");
         Ok::<_, systray::Error>(())
     })?;
 
-    app.add_menu_separator()?;*/
+    app.add_menu_separator()?;
 
     app.add_menu_item("Quit", |window| {
         window.quit();
