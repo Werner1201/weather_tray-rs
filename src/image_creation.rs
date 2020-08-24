5531cce0164ef8e4f5c527adcee41d3f051d6b51
Werner1201 <werner.romling@unigranrio.br>
2020-08-24 00:10:41 -0300

    Adding local base temperature conversion

--- src/image_creation.rs
+++ src/image_creation.rs
@@ -4,9 +4,17 @@ use rusttype::{Font, Scale};
 use std::error::Error;
 
 // Fetching data from API, returning the ready-to-display icon (or an error)
-pub fn create_icon() -> Result<Vec<u8>, Box<dyn Error>> {
+pub fn create_icon(base: i8) -> Result<Vec<u8>, Box<dyn Error>> {
     let mut img = DynamicImage::new_rgb8(256, 256);
-    let temp = crate::request_maker::get_temp()?;
+    let response = crate::request_maker::get_temp()?;
+    let mut temp = String::new();
+    if base == 0 {
+        temp = response;
+    } else if base == 1 {
+        let number: f32 = response.parse().unwrap();
+        temp = (((number * 9.0) / 5.0) + 32.0).to_string();
+    };
+
     let font = include_bytes!("../assets/DejaVuSans.ttf");
     let font = match Font::try_from_bytes(font) {
         Some(f) => f,
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ mod request_maker;
 
 //1ed014973f3aff63e2ec5bbb95751ef4
 fn main() -> Result<(), systray::Error> {
+    let mut base = 0;
     let mut app = match systray::Application::new() {
         Ok(w) => w,
         Err(_) => return Err(systray::Error::UnknownError),
@@ -15,28 +16,52 @@ fn main() -> Result<(), systray::Error> {
 
     // At app init : we create systray icon (generated from data fetched from API, or a pre-drawn error icon whatever the error)
     let error_icon = include_bytes!("../assets/error-5-16.ico");
-    let icon = match image_creation::create_icon() {
+    let icon = match image_creation::create_icon(base) {
         Ok(i) => i,
         Err(_) => error_icon.to_vec(),
     };
-    app.set_tooltip(&env::var("OPENWEATHER_LOCATION").unwrap_or("Temperature (Celcius)".to_string()))?;
+    app.set_tooltip(
+        &env::var("OPENWEATHER_LOCATION").unwrap_or("Temperature (Celcius)".to_string()),
+    )?;
     app.set_icon_from_buffer(&icon[0..icon.len()], 256, 256)?;
 
     // City change
     app.add_menu_item("Change location", move |window| {
         location_dialog();
-        let icon = match image_creation::create_icon() {
+        let icon = match image_creation::create_icon(base) {
             Ok(i) => i,
             Err(_) => error_icon.to_vec(),
         };
         window.set_icon_from_buffer(&icon[0..icon.len()], 256, 256)?;
-        window.set_tooltip(&env::var("OPENWEATHER_LOCATION").unwrap_or("Temperature (Celcius)".to_string()))?;
+        window.set_tooltip(
+            &env::var("OPENWEATHER_LOCATION").unwrap_or("Temperature (Celcius)".to_string()),
+        )?;
         Ok::<_, systray::Error>(())
     })?;
 
+    app.add_menu_item("Change to Fahrenheit", move |window| {
+        base = 1;
+        let icon = match image_creation::create_icon(base) {
+            Ok(i) => i,
+            Err(_) => error_icon.to_vec(),
+        };
+        window.set_icon_from_buffer(&icon[0..icon.len()], 256, 256)?;
+        Ok::<_, systray::Error>(())
+    })?;
+
+    app.add_menu_item("Change to Celsius", move |window| {
+        base = 0;
+        let icon = match image_creation::create_icon(base) {
+            Ok(i) => i,
+            Err(_) => error_icon.to_vec(),
+        };
+        window.set_icon_from_buffer(&icon[0..icon.len()], 256, 256)?;
+        Ok::<_, systray::Error>(())
+    })?;
     // Refresh menu : we fetch api data and update systray icon (TODO : automatic update ?)
+    //We can't do autoupdates since the API has a limit of requests per day
     app.add_menu_item("Refresh", move |window| {
-        let icon = match image_creation::create_icon() {
+        let icon = match image_creation::create_icon(base) {
             Ok(i) => i,
             Err(_) => error_icon.to_vec(),
         };
@@ -58,15 +83,15 @@ fn main() -> Result<(), systray::Error> {
 // Opens the location input window
 fn location_dialog() {
     let a = App::default();
-        let mut w = Window::new(100, 100, 300, 80, "Please select location:");
-        let city_input = Input::new(100, 30, 150, 20, "City Name:");
-        let mut b = Button::new(250, 30, 30, 20, "OK");
-        w.end();
-        w.show();
-        b.set_callback(Box::new(move || {
-            // Sets updated environment variable globally and for process
-            set_var("OPENWEATHER_LOCATION", &city_input.value()).unwrap();
-            a.quit();
-        }));
-        a.run().unwrap();
+    let mut w = Window::new(100, 100, 300, 80, "Please select location:");
+    let city_input = Input::new(100, 30, 150, 20, "City Name:");
+    let mut b = Button::new(250, 30, 30, 20, "OK");
+    w.end();
+    w.show();
+    b.set_callback(Box::new(move || {
+        // Sets updated environment variable globally and for process
+        set_var("OPENWEATHER_LOCATION", &city_input.value()).unwrap();
+        a.quit();
+    }));
+    a.run().unwrap();
 }
